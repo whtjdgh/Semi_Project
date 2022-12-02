39d178c5af47230eba3cce2dbc84e01079fac279
minseonnn <119590961+minseonnn@users.noreply.github.com>
2022-12-02 17:34:50 +0900

    ㅎㅇ

---
+++ src/main/webapp/resources/js/egan_home_header.js
@@ -0,0 +1,69 @@
+
+// 헤더 기능
+$(document).ready(function(){
+    let hd_scroll = $('.header').offset().top;
+    // // 최상단으로부터 헤더의 위치 값
+    $(window).scroll(function(){
+        let s_top = $(window).scrollTop();
+        console.log(`hd_scroll : ${hd_scroll} s_top : ${s_top}`)
+        // 최상단으로 부터 현재의 스크롤 위치 값
+
+        //스크롤 내리면 헤더 불투명
+        if(hd_scroll < s_top) { 
+            $('.header').css({
+                background :'#FFFEEB'
+            })
+
+            // $('.logo > a').css({
+            //     color: '#ffbf00'
+            // })
+
+            $('.nav > a').css({
+                color: 'black'
+            })
+            $('.h_menu > a').css({
+                color: 'black'
+            })
+            $('.h_menu::after').css({
+                borderRight: '1px solid black'
+            })
+            $('.shopping').css({
+                background: 'url(../img/장바구니_black.png)no-repeat center / 20px'
+            })
+            $('.search').css({
+                background: 'url(../img/header_search_black.png)no-repeat center / 20px'
+            })
+            
+        }  // 헤더 위치와 브라우저 스크롤 위취 값 같으면 헤더 투명
+        else if (hd_scroll == s_top) {
+            $('.header').css({
+                background :'transparent'
+            })
+            // $('.logo > a').css({
+            //     color: 'white'
+            // })
+            $('.nav > a').css({
+                color: '#ffbf00'
+            })
+            $('.h_menu > a').css({
+                color: '#ffbf00'
+            })
+            $('.h_menu::after').css({
+                borderRight: '1px solid ffbf00'
+            })
+            $('.shopping').css({
+                background: 'url(../img/장바구니_yellow.png)no-repeat center / 20px'
+            })
+            $('.search').css({
+                background: 'url(../img/header_search_yellow.png)no-repeat center / 20px'
+            })
+        } else {
+            $('.header').css({
+                background :'#FFFEEB'
+            })
+        }
+
+    })
+})
+
+
