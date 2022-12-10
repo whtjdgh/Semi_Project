3c5d60051f31ea21f8c42042e6d7b7a8a77a21c3
yh9887 <119592241+yh9887@users.noreply.github.com>
2022-12-10 23:01:15 +0900

    Update detail.js

--- src/main/webapp/resources/js/detail.js
+++ src/main/webapp/resources/js/detail.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     $(document).on('click', '#log_cancle_btn', function() {
         $('.logChk_popup').css({display:'none'})
         $('.layer').css({display:'none'})
-        $('html').css({overflowY:'visible'})
+//         $('html').css({overflowY:'visible'})
     })
     $(document).on('click', '#logChk_commit_btn', function() {
         location.href="http://localhost:8080/egan/login"
